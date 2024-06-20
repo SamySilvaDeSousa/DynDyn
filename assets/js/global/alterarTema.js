76a95c836eba16803a30178f46f26e6ca6541a1b
Samy Silva <samysilvadesousa@gmail.com>
2024-06-20 20:12:23 -0300

    Protótipo das Transações E Contas feitos

--- assets/js/global/alterarTema.js
+++ assets/js/global/alterarTema.js
@@ -1,9 +1,26 @@
-document.getElementById('botao-tema').addEventListener('click', function() {
-  document.body.classList.toggle('tema-dark');
-});
+    // Função para aplicar o tema com base na preferência armazenada
+    function aplicarTema() {
+      const tema = localStorage.getItem('tema');
+      if (tema === 'dark') {
+        document.body.classList.add('tema-dark');
+        document.getElementById('check-tema').checked = true;
+      }
+    }
 
-const check = document.getElementById('check-tema')
+    // Aplica o tema ao carregar a página
+    aplicarTema();
 
-check.addEventListener('change', () => {
-  document.body.classList.toggle('tema-dark')
-})
+    document.getElementById('botao-tema').addEventListener('click', function() {
+      document.body.classList.toggle('tema-dark');
+      // Armazenar a preferência do usuário
+      const tema = document.body.classList.contains('tema-dark') ? 'dark' : 'light';
+      localStorage.setItem('tema', tema);
+    });
+
+    const check = document.getElementById('check-tema');
+    check.addEventListener('change', () => {
+      document.body.classList.toggle('tema-dark');
+      // Armazenar a preferência do usuário
+      const tema = document.body.classList.contains('tema-dark') ? 'dark' : 'light';
+      localStorage.setItem('tema', tema);
+    });
--- assets/js/global/sidebar.js
+++ assets/js/global/sidebar.js
@@ -1,25 +1,12 @@
 document.querySelectorAll('.nav-link').forEach(link => {
   link.addEventListener('click', function() {
 
-    const cortina = document.querySelector('.cortina');
-    cortina.style.display = 'block';
-    cortina.style.animation = 'none';
-
-    void cortina.offsetWidth;
-    cortina.style.animation = 'cortinas 0.5s ease-in-out forwards';
-
     document.querySelectorAll('.nav-link').forEach(nav => nav.classList.remove('ativo'));
+    
     document.querySelectorAll('.section').forEach(section => section.style.display = 'none');
     
     this.classList.add('ativo');
-    
-    setTimeout(() => {
-      const sectionId = this.id.replace('nav-', '');
-      document.getElementById(sectionId).style.display = 'flex';
-
-      setTimeout(() => {
-        cortina.style.display = 'none';
-      }, 250);
-    }, 250);
+    const sectionId = this.id.replace('nav-', '');
+    document.getElementById(sectionId).style.display = 'flex';
   });
 });
